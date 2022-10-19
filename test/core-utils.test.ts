0b29e4ccf685fd1083684595671639a6058946c0
Saya <chu121su12@gmail.com>
2022-10-19 22:32:38 +0800

    wip

--- test/core-utils.test.ts
+++ test/core-utils.test.ts
@@ -1,5 +1,14 @@
-import { mergeDeep } from '@unocss/core'
+import { createGenerator, mergeDeep } from '@unocss/core'
+import type { Theme } from '@unocss/preset-mini';
+import { presetMini } from 'unocss';
 import { expect, test } from 'vitest'
+
+const mergeTheme = (toMerge: any): Theme => {
+  return createGenerator({
+    presets: [presetMini()],
+    theme: toMerge,
+  }).config.theme ?? {}
+}
 
 test('utils object mergeDeep', () => {
   expect(mergeDeep({}, {})).eql({})
@@ -7,4 +16,65 @@ test('utils object mergeDeep', () => {
   expect(mergeDeep({ a: { b: 1, c: 2 } }, { a: { b: 3 } })).eql({ a: { b: 3, c: 2 } })
   expect(mergeDeep({ a: { b: 1, c: 2 } }, { a: () => ({ c: 3 }) })).eql({ a: { c: 3 } })
   expect(mergeDeep({ a: { b: 1, c: 2 } }, { a: bc => ({ a: 0, ...bc, c: 3 }) })).eql({ a: { a: 0, b: 1, c: 3 } })
+
+  expect(Object.entries(mergeTheme({}).breakpoints ?? {})).eql([
+    ['sm', '640px'],
+    ['md', '768px'],
+    ['lg', '1024px'],
+    ['xl', '1280px'],
+    ['2xl', '1536px'],
+  ])
+
+  expect(Object.entries(mergeTheme({
+    breakpoints: {
+      'large': '1000px',
+    },
+  }.breakpoints ?? {})).eql([
+    ['sm', '640px'],
+    ['md', '768px'],
+    ['lg', '1024px'],
+    ['xl', '1280px'],
+    ['2xl', '1536px'],
+    ['large', '1000px'],
+  ])
+
+  expect(Object.entries(mergeTheme('breakpoints', {
+    breakpoints: () => ({
+      'large': '1000px',
+    }),
+  }.breakpoints ?? {})).eql([
+    ['large', '1000px'],
+  ])
+
+  expect(Object.entries(mergeTheme('breakpoints', {
+    breakpoints: (prev: any) => ({
+      'large': '1000px',
+      'xl': prev.xl,
+      'larger': '2000px',
+    }),
+  }.breakpoints ?? {})).eql([
+    ['large', '1000px'],
+    ['xl', '1280px'],
+    ['larger', '2000px'],
+  ])
+
+  expect(Object.entries(mergeTheme('breakpoints', {
+    breakpoints: () => ({
+      'large': '1000px',
+    }),
+  }.breakpoints ?? {})).eql([
+    ['large', '1000px'],
+  ])
+
+  expect(Object.entries(mergeTheme('breakpoints', {
+    breakpoints: (prev: any) => ({
+      'large': '1000px',
+      'xl': prev.xl,
+      'larger': '2000px',
+    }),
+  }.breakpoints ?? {})).eql([
+    ['large', '1000px'],
+    ['xl', '1280px'],
+    ['larger', '2000px'],
+  ])
 })
