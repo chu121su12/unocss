5c7fbdcfabb44044cd463ca7c6eeec71414eafee
Saya <chu121su12@gmail.com>
2023-12-05 09:35:24 +0800

    feat(preset-mini): add gap-col/row and deprecate gap-x/y

--- packages/preset-mini/src/_rules/gap.ts
+++ packages/preset-mini/src/_rules/gap.ts
@@ -1,3 +1,4 @@
+import { warnOnce } from '@unocss/core'
 import type { Rule, RuleContext } from '@unocss/core'
 import type { Theme } from '../theme'
 import { h } from '../utils'
@@ -5,7 +6,9 @@ import { h } from '../utils'
 const directions: Record<string, string> = {
   '': '',
   'x': 'column-',
+  'col': 'column-',
   'y': 'row-',
+  'row': 'row-',
 }
 
 function handleGap([, d = '', s]: string[], { theme }: RuleContext<Theme>) {
@@ -19,5 +22,9 @@ function handleGap([, d = '', s]: string[], { theme }: RuleContext<Theme>) {
 
 export const gaps: Rule[] = [
   [/^(?:flex-|grid-)?gap-?()(.+)$/, handleGap, { autocomplete: ['gap-$spacing', 'gap-<num>'] }],
-  [/^(?:flex-|grid-)?gap-([xy])-?(.+)$/, handleGap, { autocomplete: ['gap-(x|y)-$spacing', 'gap-(x|y)-<num>'] }],
+  [/^(?:flex-|grid-)?gap-([xy])-?(.+)$/, (...args) => {
+    warnOnce('The rule gap with x/y modifier is deprecated and will be removed in future version. Please use the col/row modifier instead')
+    return handleGap(...args)
+  }, { autocomplete: ['gap-(x|y)-$spacing', 'gap-(x|y)-<num>'] }],
+  [/^(?:flex-|grid-)?gap-(col|row)-?(.+)$/, handleGap, { autocomplete: ['gap-(col|row)-$spacing', 'gap-(col|row)-<num>'] }],
 ]
